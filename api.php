37e5edf2da86316a937f33a1d34526fc5022a0ce
Alex Aragon <aragonwa@gmail.com>
2015-02-22 20:05:14 -0800

    Clean up of files

--- api.php
+++ api.php
@@ -1,26 +1,19 @@
 <?php
  /**
- * api.php is a sample API file to be called via AJAX to 
- * deliver the contents of 2 JSON files, one of which orders the 
- * data by year, the other by Box Office Results.
  *
  * The page can be called via AJAX either via GET or POST, using a variable 
  * named "cat":
  *
  * <code>
- * api.php?cat=box
+ * api.php?cat=artist
  * </code>
- *
- * In the example above, the parameter cat is loaded with the string "box" 
- * which will indicate to the API to load the JSON file containing 
- * box office results.
  *
  * @package art-works-web-service
  * @author Alex Aragon
  * @version 1.01 2015/02/22
  * @link 
  * @license http://www.apache.org/licenses/LICENSE-2.0
- * @see index.htm
+ * @see index.html
  * @todo none
  */
 
@@ -31,6 +24,9 @@ if(isset($_REQUEST['cat']))
 		case "artist":
 			include('data/artist-data.js'); 
 			break;
+    case "title":
+      include('data/title-data.js'); 
+      break;
 		default:
 			include('data/title-data.js'); 
 	}
